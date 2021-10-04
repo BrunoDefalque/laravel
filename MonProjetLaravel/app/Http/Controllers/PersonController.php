b2f496c4c07240cfd11e99064cbad05c4bfaf010
BrunoDefalque <82031231+BrunoDefalque@users.noreply.github.com>
2021-10-04 18:59:56 +0200

    Ajout methode publique nommee hi dans le controleur

--- MonProjetLaravel/app/Http/Controllers/PersonController.php
+++ MonProjetLaravel/app/Http/Controllers/PersonController.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -8,3 +10,15 @@ class PersonController extends Controller
 {
     //
 }
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    public function hi(){
+        return "Bonjour cher visiteur !";
+    }
+}
+
